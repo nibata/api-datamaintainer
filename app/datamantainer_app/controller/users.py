ee9715c3e65d4ea673a818d0ed25c936584b1b4e
Nicolás Bacquet <nibata@gmail.com>
2023-01-14 14:10:28 -0300

    se llevan schemas a carpeta diferente

--- app/datamantainer_app/controller/users.py
+++ app/datamantainer_app/controller/users.py
@@ -1,25 +1,25 @@
 from sqlalchemy.orm import Session
 
-from ..models import users
+from ..models import users as model_users
  
-from .. import schemas
+from ..schemas import users as schema_users
  
  
 def get_user(db: Session, user_id: int):
-    return db.query(users.Users).filter(users.Users.id == user_id).first()
+    return db.query(model_users.Users).filter(model_users.Users.id == user_id).first()
  
  
 def get_user_by_email(db: Session, email: str):
-    return db.query(users.Users).filter(users.Users.email == email).first()
+    return db.query(model_users.Users).filter(model_users.Users.email == email).first()
  
  
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(users.Users).offset(skip).limit(limit).all()
+    return db.query(model_users.Users).offset(skip).limit(limit).all()
  
  
-def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: schema_users.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
-    db_user = users.Users(email=user.email, hashed_password=fake_hashed_password)
+    db_user = model_users.Users(email=user.email, hashed_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
--- app/datamantainer_app/main.py
+++ app/datamantainer_app/main.py
@@ -7,8 +7,8 @@ from .controller import users as controller_user
 
 from .models import users as model_user
  
-from . import schemas
-from .database import SessionLocal, engine
+from .schemas import users as schema_users
+from .database import SessionLocal
  
 #users.Base.metadata.create_all(bind=engine)
  
@@ -24,21 +24,21 @@ def get_db():
         db.close()
  
  
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+@app.post("/users/", response_model=schema_users.User)
+def create_user(user: schema_users.UserCreate, db: Session = Depends(get_db)):
     db_user = controller_user.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return controller_user.create_user(db=db, user=user)
  
  
-@app.get("/users/", response_model=List[schemas.User])
+@app.get("/users/", response_model=List[schema_users.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = controller_user.get_users(db, skip=skip, limit=limit)
     return users
  
  
-@app.get("/users/{user_id}", response_model=schemas.User)
+@app.get("/users/{user_id}", response_model=schema_users.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = controller_user.get_user(db, user_id=user_id)
     if db_user is None:
