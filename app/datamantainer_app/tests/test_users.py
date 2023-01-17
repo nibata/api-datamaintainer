34b56b74c2755a1258bdb21268fded7994cb6ef4
Nicolás Bacquet <nibata@gmail.com>
2023-01-16 22:44:23 -0300

    se prueba con test unitarios

--- app/datamantainer_app/tests/test_users.py
+++ app/datamantainer_app/tests/test_users.py
@@ -11,6 +11,17 @@ def test_check_life_of_users_route():
     assert response.status_code == 200
 
 
+def test_create_user():
+    json = {
+        "email": "nibata@gmail.com",
+        "password": "pwd_test"
+    }
+
+    response = client.post("/users", json=json)
+
+    assert response.status_code == 200
+
+
 def test_get_user_nibata_in_id_1():
     response = client.get("/users?user_id=1")
 
@@ -19,18 +30,3 @@ def test_get_user_nibata_in_id_1():
         "id": 1, 
         "is_active": False
     }]
-
-
-def test_create_existing_user():
-    json = {
-        "email": "nibata@gmail.com",
-        "password": "pwd_test"
-    }
-
-    response = client.post("/users", json= json)
-
-    assert response.status_code == 400
-
-    assert response.json() == {
-        "detail": "Email already registered"
-    }
