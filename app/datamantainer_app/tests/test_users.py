37ab9393b5f8126b31512593a7d1ed10b2fa615b
Nicolás Bacquet <nibata@gmail.com>
2023-03-12 17:30:33 -0300

    se corrigen test unitarios

--- app/datamantainer_app/tests/test_users.py
+++ app/datamantainer_app/tests/test_users.py
@@ -11,22 +11,24 @@ def test_check_life_of_users_route():
     assert response.status_code == 200
 
 
-def test_create_user():
+def test_create_user_without_authorization():
     json = {
+        "fullname": "Nicolás Bacquet",
         "email": "nibata@gmail.com",
         "password": "pwd_test"
     }
 
     response = client.post("/users", json=json)
 
-    assert response.status_code == 200
+    assert response.status_code == 403
 
 
 def test_get_user_nibata_in_id_1():
-    response = client.get("/users?user_id=1")
-
-    assert response.json() == [{
+    response = client.get("/users/q?user_id=1")
+    print(response.json())
+    assert response.json() == {
+        "fullname": "Nicolás Bacquet",
         "email": "nibata@gmail.com", 
         "id": 1, 
         "is_active": False
-    }]
+    }
