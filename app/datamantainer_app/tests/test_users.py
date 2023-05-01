38369a7a364269d0070dd42516a6668fbd4b8abd
Nicolás Bacquet <nibata@gmail.com>
2023-04-30 20:58:24 -0400

    cambio en test unitario

--- app/datamantainer_app/tests/test_users.py
+++ app/datamantainer_app/tests/test_users.py
@@ -42,7 +42,7 @@ def test_create_user():
          "fullname": "test",
          "email": "test@test.test", 
          "password": "pwd_test",
-         "expiration_date": "2023-05-01"
+         "expiration_date": None
     }
 
     response_token = client.post("/user/login", json=json_token)
