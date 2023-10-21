630d4bc4cebbae13d6c9c844901ad8d4d644bf93
Nicolás Bacquet <nibata@gmail.com>
2023-10-21 11:02:03 -0300

    prueba de unique constraint mediante alembic post creación de la tabla

--- app/datamantainer_app/models/authentication/users_groups.py
+++ app/datamantainer_app/models/authentication/users_groups.py
@@ -1,11 +1,12 @@
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field, Relationship, UniqueConstraint
 from .groups import Group
 from .users import User
 
 
 class UserGroupLink(SQLModel, table=True):
     __tablename__ = "user_group_link"
-    __table_args__ = {"schema": "authentication"}
+    __table_args__ = (UniqueConstraint("user_id", "group_id", name="unique_user_group_constraint"),
+                      {"schema": "authentication"})
 
     # Fields
     user_id: int = Field(foreign_key=User.id, primary_key=True)
