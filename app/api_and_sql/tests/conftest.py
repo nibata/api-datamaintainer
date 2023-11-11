ea14909c559556307a54db611b13b4361f4a8115
Nicolás Bacquet <nibata@gmail.com>
2023-11-11 12:29:11 -0300

    prueba de test unitario 6

--- app/api_and_sql/tests/conftest.py
+++ app/api_and_sql/tests/conftest.py
@@ -13,10 +13,20 @@ from alembic import context
 def db():
     # Create a test database engine
     uri_async_db = os.environ.get("DB_ASYNC_TEST")
+    DB_DRIVER = os.environ.get("DB_DRIVER")
+    DB_ASYNC_DRIVER = os.environ.get("DB_ASYNC_DRIVER")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASS = os.environ.get("DB_PASS")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_DATABASE_NAME = os.environ.get("DB_DATABASE_NAME")
+    SQLALCHEMY_DATABASE_URL = f"{DB_DRIVER}+{DB_ASYNC_DRIVER}://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_DATABASE_NAME}"
+
     print("#" * 10)
     print("#" * 10)
     print("#" * 10)
-    print(uri_async_db )
+    print(uri_async_db)
+    print(SQLALCHEMY_DATABASE_URL)
     print("#" * 10)
     print("#" * 10)
     print("#" * 10)
@@ -38,3 +48,6 @@ def db():
         session.close()
         engine.dispose()
         command.downgrade(alembic_cfg, "base")
+
+    else:
+        engi
