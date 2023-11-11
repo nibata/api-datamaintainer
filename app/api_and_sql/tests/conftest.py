c35311ab7a103e65938aabd313a9a8a5c82c82d0
Nicolás Bacquet <nibata@gmail.com>
2023-11-11 12:17:33 -0300

    prueba de test unitario 3

--- app/api_and_sql/tests/conftest.py
+++ app/api_and_sql/tests/conftest.py
@@ -13,20 +13,21 @@ from alembic import context
 def db():
     # Create a test database engine
     uri_async_db = os.environ.get("DB_ASYNC_TEST")
-    engine = create_engine(uri_async_db)
-    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    if uri_async_db is not None or uri_async_db != "":
+        engine = create_engine(uri_async_db)
+        Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-    # Run Alembic migrations
-    alembic_cfg = Config("alembic.ini")
-    alembic_cfg.set_main_option("script_location", "alembic")
-    command.upgrade(alembic_cfg, "head")
+        # Run Alembic migrations
+        alembic_cfg = Config("alembic.ini")
+        alembic_cfg.set_main_option("script_location", "alembic")
+        command.upgrade(alembic_cfg, "head")
 
-    # Create a new session for the tests
-    session = Session()
+        # Create a new session for the tests
+        session = Session()
 
-    yield session
+        yield session
 
-    # Teardown: Drop the test database
-    session.close()
-    engine.dispose()
-    command.downgrade(alembic_cfg, "base")
+        # Teardown: Drop the test database
+        session.close()
+        engine.dispose()
+        command.downgrade(alembic_cfg, "base")
